426e0d71a17a41a6d15e4a1fa8378f5f048b700e
engin7916ai <75510889+engin7916ai@users.noreply.github.com>
2021-05-24 13:58:09 +0300

    Initialize bogavril/msa-pt-3

---
+++ src/client/Microsoft.Identity.Client/Internal/Broker/NullBroker.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Identity.Client.ApiConfig.Parameters;
+using Microsoft.Identity.Client.Internal.Requests;
+using Microsoft.Identity.Client.OAuth2;
+using Microsoft.Identity.Client.UI;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microsoft.Identity.Client.Internal.Broker
+{
+    /// <summary>
+    /// For platforms that do not support a broker
+    /// </summary>
+    internal class NullBroker : IBroker
+    {
+        public bool IsBrokerInstalledAndInvokable()
+        {
+            return false;
+        }      
+
+        public Task<MsalTokenResponse> AcquireTokenInteractiveAsync(AuthenticationRequestParameters authenticationRequestParameters, AcquireTokenInteractiveParameters acquireTokenInteractiveParameters)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public Task<MsalTokenResponse> AcquireTokenSilentAsync(AuthenticationRequestParameters authenticationRequestParameters, AcquireTokenSilentParameters acquireTokenSilentParameters)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public void HandleInstallUrl(string appLink)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public Task<IEnumerable<IAccount>> GetAccountsAsync(string clientID, string redirectUri)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public Task RemoveAccountAsync(IAppConfigInternal appConfig, IAccount account)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public Task<MsalTokenResponse> AcquireTokenSilentDefaultUserAsync(AuthenticationRequestParameters authenticationRequestParameters, AcquireTokenSilentParameters acquireTokenSilentParameters)
+        {
+            throw new PlatformNotSupportedException();
+        }
+    }
+}
---
+++ src/client/Microsoft.Identity.Client/Platforms/Features/WamBroker/WamExtensions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.Web.Core;
+
+namespace Microsoft.Identity.Client.Platforms.Features.WamBroker
+{
+    internal static class WamExtensions
+    {
+        public static bool IsSuccessResponse(this WebTokenRequestStatus status)
+        {
+            return status == WebTokenRequestStatus.Success ||
+                status == WebTokenRequestStatus.AccountSwitch;
+        }
+    }
+}
