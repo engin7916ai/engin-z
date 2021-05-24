c202477c22ceba6c642e57c653e56f1b9b7534ae
engin7916ai <75510889+engin7916ai@users.noreply.github.com>
2021-05-24 13:57:19 +0300

    Initialize bogavril/poprename

---
+++ src/client/Microsoft.Identity.Client/AuthenticationResultMetadata.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Identity.Client
+{
+    /// <summary>
+    /// Contains metadata of the authentication result.
+    /// </summary>
+    public class AuthenticationResultMetadata
+    {
+
+        /// <summary>
+        /// Constructor for the class AuthenticationResultMetadata
+        /// <param name="tokenSource">The token source.</param>
+        /// </summary>
+        public AuthenticationResultMetadata(TokenSource tokenSource)
+        {
+            TokenSource = tokenSource;
+        }
+
+        /// <summary>
+        /// The source of the token in the result.
+        /// </summary>
+        public TokenSource TokenSource { get; }
+    }
+}
---
+++ tests/Microsoft.Identity.Test.Unit/ApiConfigTests/AcquireTokenOnBehalfOfBuilderTests.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Microsoft.Identity.Test.Common;
+using Microsoft.Identity.Test.Unit.ApiConfigTests.Harnesses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Identity.Test.Unit.ApiConfigTests
+{
+    [TestClass]
+    [TestCategory("BuilderTests")]
+    public class AcquireTokenOnBehalfOfBuilderTests
+    {
+        private AcquireTokenOnBehalfOfBuilderHarness _harness;
+
+        [TestInitialize]
+        public async Task TestInitializeAsync()
+        {
+            TestCommon.ResetInternalStaticCaches();
+            _harness = new AcquireTokenOnBehalfOfBuilderHarness();
+            await _harness.SetupAsync()
+                          .ConfigureAwait(false);
+        }
+    }
+}
