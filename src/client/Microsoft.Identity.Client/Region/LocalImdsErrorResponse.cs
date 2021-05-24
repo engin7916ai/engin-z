a4aa6c0d6a20b87ec2bb037f1504313961095af4
engin7916ai <75510889+engin7916ai@users.noreply.github.com>
2021-05-24 13:58:18 +0300

    Initialize trwalke/demo

---
+++ src/client/Microsoft.Identity.Client/Region/LocalImdsErrorResponse.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Identity.Json;
+
+namespace Microsoft.Identity.Client.Region
+{
+    [JsonObject]
+    [Preserve(AllMembers = true)]
+    internal sealed class LocalImdsErrorResponse
+    {
+        [JsonProperty(PropertyName = "error")]
+        public string Error { get; set; }
+
+        [JsonProperty(PropertyName = "newest-versions")]
+        public List<string> NewestVersions { get; set; }
+    }
+}
+
+
---
+++ tests/devapps/XForms/XForms.iOS/AcquirePageRenderer.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Xamarin.Forms.Platform.iOS;
+
+namespace XForms.iOS
+{
+    internal class AcquirePageRenderer : PageRenderer
+    {
+
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+#if BUILDENV == APPCENTER
+            Xamarin.Calabash.Start();
+#endif
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+    }
+}
