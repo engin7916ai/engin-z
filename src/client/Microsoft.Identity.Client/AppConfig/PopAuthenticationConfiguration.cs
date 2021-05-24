b35160aa9096e03956ac5c7a53a029b7d65226b4
engin7916ai <75510889+engin7916ai@users.noreply.github.com>
2021-05-24 13:57:16 +0300

    Initialize bogavril/popspell

---
+++ src/client/Microsoft.Identity.Client/AppConfig/PopAuthenticationConfiguration.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.Identity.Client.AuthScheme.PoP;
+
+namespace Microsoft.Identity.Client.AppConfig
+{
+
+    /// <summary>
+    /// Configuration properties used to construct a proof of possession request.
+    /// </summary>
+#if !SUPPORTS_CONFIDENTIAL_CLIENT
+    [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+#endif
+    public class PoPAuthenticationConfiguration
+    {
+        /// <summary>
+        /// Constructs the configuration properties used to construct a proof of possession request
+        /// </summary>
+        /// <param name="requestUri"> The Uri of the protected resource which requires a PoP token. The PoP token will be cryptographically bound to it, i.e. to the host and path portions of this uri</param>
+        public PoPAuthenticationConfiguration(Uri requestUri)
+        {
+            ConfidentialClientApplication.GuardMobileFrameworks();
+            RequestUri = requestUri ?? throw new ArgumentNullException(nameof(requestUri));
+        }
+
+        /// <summary>
+        /// The Uri of the protected resource which requires a PoP token. The PoP token will be cryptographically bound to it, i.e. to the host and path portions of this uri
+        /// </summary>
+        public Uri RequestUri { get; }
+
+        /// <summary>
+        /// The HTTP method ("GET", "POST" etc.) method that will be bound to the token. Leave null and the POP token will not be bound to the method.
+        /// </summary>
+        public HttpMethod HttpMethod { get; set; }
+
+        /// <summary>
+        /// An extensibility point that allows developers to define their own key management. 
+        /// Leave <c>null</c> and MSAL will use a default implementation, which generates an RSA key pair in memory and refreshes it every 8 hours.
+        /// Important note: if you want to change the key (e.g. rotate the key), you should create a new instance of this object,
+        /// as MSAL.NET will keep a thumbprint of keys in memory.
+        /// </summary>
+        public IPoPCryptoProvider PopCryptoProvider { get; set; }
+    }
+}
---
+++ tests/Microsoft.Identity.Test.Unit/CacheTests/LoadingProjectsTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Identity.Client;
+using Microsoft.Identity.Client.Core;
+using Microsoft.Identity.Test.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Identity.Test.Unit.CacheTests
+{
+#if !ANDROID && !iOS && !WINDOWS_APP // custom token cache serialization not available 
+    [TestClass]
+    public class LoadingProjectsTests
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestCommon.ResetInternalStaticCaches();
+        }
+
+        [TestMethod]
+        public void CanDeserializeTokenCache()
+        {
+            TokenCache tokenCache = new TokenCache(TestCommon.CreateDefaultServiceBundle(), false)
+            {
+                AfterAccess = args => { Assert.IsFalse(args.HasStateChanged); }
+            };
+
+            ((ITokenCacheSerializer)tokenCache).DeserializeMsalV3(null);
+#pragma warning disable CS0618 // Type or member is obsolete
+            Assert.IsFalse(tokenCache.HasStateChanged, "State should not have changed when deserializing nothing.");
+#pragma warning restore CS0618 // Type or member is obsolete
+            
+        }
+    }
+#endif
+}
