3708e3810265832020409fbbc09a30884e2a88fe
engin7916ai <75510889+engin7916ai@users.noreply.github.com>
2021-05-24 13:56:29 +0300

    Initialize bogavril/spike/webview2

---
+++ tests/CacheCompat/CommonCache.Test.Unit/Utils/CacheProgramType.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace CommonCache.Test.Unit.Utils
+{
+    public enum CacheProgramType
+    {
+        AdalV3,
+        AdalV4,
+        AdalV5,
+        MsalV2,
+        MsalV3,
+        MsalPython,
+        MsalJava
+    }
+}
