93f9dba4e4d669fee294f37ec5d59dc82bd479a4
engin7916ai <75510889+engin7916ai@users.noreply.github.com>
2021-05-24 13:56:32 +0300

    Initialize PopCCA-2

---
+++ tests/Microsoft.Identity.Test.Unit.net45/pop/CryptoProviderTests.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+#if DESKTOP
+
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Identity.Client.Platforms.net45;
+using Microsoft.Identity.Client.PlatformsCommon;
+using Microsoft.Identity.Json.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Identity.Test.Unit.PoP
+{
+
+    [TestClass]
+    public class NetDesktopPoPCryptoProviderTests
+    {
+
+        [TestMethod]
+        public void ValidateCannonicalJwk()
+        {
+            var provider = new NetSharedPoPCryptoProvider();
+            string jwk = provider.CannonicalPublicKeyJwk;
+            dynamic jwkObj = JObject.Parse(jwk);
+
+            Assert.IsNotNull(jwkObj.E);
+            Assert.IsNotNull(jwkObj.N);
+            Assert.AreEqual("RSA", jwkObj.kty.ToString());
+        }
+    }
+}
+#endif
