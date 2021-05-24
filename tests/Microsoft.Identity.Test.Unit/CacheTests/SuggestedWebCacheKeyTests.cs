693c87a9cea386607d3b8f3d79dc4f94bc315d5d
engin7916ai <75510889+engin7916ai@users.noreply.github.com>
2021-05-24 13:59:17 +0300

    Initialize pmaytak/wam-account-picker-parent-repro

---
+++ tests/Microsoft.Identity.Test.Unit/CacheTests/SuggestedWebCacheKeyTests.cs
@@ -0,0 +1,90 @@
+using System;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Client.ApiConfig.Parameters;
+using Microsoft.Identity.Client.Cache;
+using Microsoft.Identity.Client.Instance;
+using Microsoft.Identity.Client.Internal;
+using Microsoft.Identity.Client.Internal.Requests;
+using Microsoft.Identity.Client.TelemetryCore.Internal.Events;
+using Microsoft.Identity.Test.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Identity.Test.Unit.CacheTests
+{
+    [TestClass]
+    public class SuggestedWebCacheKeyTests
+    {
+        private IServiceBundle _serviceBundle;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this._serviceBundle = TestCommon.CreateDefaultServiceBundle();
+        }
+
+        [TestMethod]
+        public void TestCacheKeyForADFSAuthority()
+        {
+            // Arrange
+            var appTokenCache = new TokenCache(this._serviceBundle, isApplicationTokenCache: true);
+            var requestContext = new RequestContext(this._serviceBundle , Guid.NewGuid());
+            var authority = Authority.CreateAuthority(TestConstants.ADFSAuthority, true);
+
+            requestContext.ServiceBundle.Config.AuthorityInfo = authority.AuthorityInfo;
+                
+
+
+            var acquireTokenCommonParameters = new AcquireTokenCommonParameters
+            {
+                ApiId = ApiEvent.ApiIds.AcquireTokenForClient,                
+            };
+
+            var parameters = new AuthenticationRequestParameters(
+                this._serviceBundle,
+                appTokenCache,
+                acquireTokenCommonParameters, 
+                requestContext,
+                authority);
+
+
+            // Act
+            var actualKey = SuggestedWebCacheKeyFactory.GetKeyFromRequest(parameters);
+
+            // Assert
+            Assert.IsNotNull(actualKey);
+            var expectedKey = $"{this._serviceBundle.Config.ClientId}__AppTokenCache";
+            Assert.AreEqual(expectedKey, actualKey);
+        }
+
+        [TestMethod]
+        public void TestCacheKeyForTenantAuthority()
+        {
+            // Arrange
+            const string tenantId = TestConstants.AadTenantId;
+            var appTokenCache = new TokenCache(this._serviceBundle, isApplicationTokenCache: true);
+            var requestContext = new RequestContext(this._serviceBundle , Guid.NewGuid());
+            var tenantAuthority = AuthorityInfo.FromAadAuthority(AzureCloudInstance.AzurePublic, tenant: tenantId, validateAuthority: false);
+            var acquireTokenCommonParameters = new AcquireTokenCommonParameters
+            {
+                ApiId = ApiEvent.ApiIds.AcquireTokenForClient,
+                AuthorityOverride = tenantAuthority
+            };
+
+            var parameters = new AuthenticationRequestParameters(
+                this._serviceBundle,
+                appTokenCache,
+                acquireTokenCommonParameters, 
+                requestContext, 
+                Authority.CreateAuthority(tenantAuthority));
+
+
+            // Act
+            var actualKey = SuggestedWebCacheKeyFactory.GetKeyFromRequest(parameters);
+
+            // Assert
+            Assert.IsNotNull(actualKey);
+            var expectedKey = $"{this._serviceBundle.Config.ClientId}_{tenantId}_AppTokenCache";
+            Assert.AreEqual(expectedKey, actualKey);
+        }
+    }
+}
---
+++ tests/devapps/WAM/NetFrameworkWam/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetDesktopWinForms
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            IntPtr childAppHandle = args.Length > 0 ? (IntPtr)long.Parse(args[0]) : IntPtr.Zero;
+            Application.Run(new Form1(childAppHandle));
+        }
+    }
+}
