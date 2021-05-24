4646c64fe3c2571c0686ecdb847c1dd71238e461
engin7916ai <75510889+engin7916ai@users.noreply.github.com>
2021-05-24 13:58:21 +0300

    Initialize bogavril/wv2

---
+++ src/client/Microsoft.Identity.Client.Desktop/MsalDesktopWebUiFactory.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Microsoft.Identity.Client.ApiConfig.Parameters;
+using Microsoft.Identity.Client.Internal;
+using Microsoft.Identity.Client.Platforms.Features.WebView2WebUi;
+using Microsoft.Identity.Client.Platforms.Shared.Desktop.OsBrowser;
+using Microsoft.Identity.Client.UI;
+using Microsoft.Web.WebView2.Core;
+
+namespace Microsoft.Identity.Client.Desktop
+{
+    internal class MsalDesktopWebUiFactory : IWebUIFactory
+    {
+        private readonly bool _fallbackToLegacyWebBrowser;
+        private readonly Func<bool> _isWebView2AvailableFunc;
+
+        public MsalDesktopWebUiFactory(
+            bool fallbackToLegacyWebBrowser = false,
+            Func<bool> isWebView2AvailableForTest = null)
+        {
+            _fallbackToLegacyWebBrowser = fallbackToLegacyWebBrowser;
+            _isWebView2AvailableFunc = isWebView2AvailableForTest ?? IsWebView2Available;
+        }
+
+        public bool IsSystemWebViewAvailable => true;
+
+        public IWebUI CreateAuthenticationDialog(CoreUIParent coreUIParent, WebViewPreference useEmbeddedWebView, RequestContext requestContext)
+        {
+            if (useEmbeddedWebView == WebViewPreference.System)
+            {
+                requestContext.Logger.Info("Using system browser");
+                return new DefaultOsBrowserWebUi(
+                    requestContext.ServiceBundle.PlatformProxy,
+                    requestContext.Logger,
+                    coreUIParent.SystemWebViewOptions);
+            }
+
+            if (_isWebView2AvailableFunc())
+            {
+                requestContext.Logger.Info("Using WebView2 embedded browser");
+                return new WebView2WebUi(coreUIParent, requestContext);
+            }
+
+#if DESKTOP
+            if (_fallbackToLegacyWebBrowser)
+            {
+                requestContext.Logger.Info("Using legacy embedded browser");
+                return 
+                    new Microsoft.Identity.Client.Platforms.Features.WinFormsLegacyWebUi.InteractiveWebUI(coreUIParent, requestContext);
+            }
+#endif
+            throw new MsalClientException(
+                MsalError.WebView2NotInstalled,
+                "The embedded browser needs WebView2 runtime to be installed. If you are an end user of the app, please download and install the WebView2 runtime from https://go.microsoft.com/fwlink/p/?LinkId=2124703 and restart the app." +
+                " If you are an app developer, please ensure that your app installs the WebView2 runtime https://docs.microsoft.com/en-us/microsoft-edge/webview2/concepts/distribution");
+        }
+
+        private bool IsWebView2Available()
+        {
+            string wv2Version = CoreWebView2Environment.GetAvailableBrowserVersionString();
+            return !string.IsNullOrEmpty(wv2Version);
+        }
+    }
+}
---
+++ tests/CacheCompat/CommonCache.Test.Common/ProcessRunResults.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace CommonCache.Test.Common
+{
+    public sealed class ProcessRunResults
+    {
+        public ProcessRunResults(string standardOut, string standardError)
+        {
+            StandardOut = standardOut;
+            StandardError = standardError;
+        }
+
+        public string StandardOut { get; }
+        public string StandardError { get; }
+    }
+}
